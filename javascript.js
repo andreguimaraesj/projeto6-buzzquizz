887b684564b4d8ac1c33afa8791c0a992e18abbd
andreguimaraesj <dev.detto@gmail.com>
2023-04-20 00:12:05 -0300

    começo

--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
 axios.defaults.headers.common['Authorization'] = 'TumGlLJcWaHWS4AyRV2AOZCy';
 let boxTodosOsQuizzes = document.querySelector('.todosQuizzes');
+let quizzClicado = {};
+
+
+
+
 
 
 
@@ -8,15 +13,25 @@ function renderizaQuizzesDoServidor(){
     todosQuizzesDoServidor = axios.get("https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes");
     todosQuizzesDoServidor.then((quiz) =>{
         todosQuizzesDoServidor = quiz.data;
+       console.log(todosQuizzesDoServidor);
 
         for(let i = 0; i < todosQuizzesDoServidor.length; i++){
-            boxTodosOsQuizzes.innerHTML += `<div class="quizzDoServidor">
+            boxTodosOsQuizzes.innerHTML += `<div class="quizzDoServidor" onclick="abreQuizz(${todosQuizzesDoServidor[i].id})">
                 <div class="tituloDeCadaQuizzDoServidor">${todosQuizzesDoServidor[i].title}</div>
-                <img class="imagemQuizzesDoServidor" src="${todosQuizzesDoServidor[i].image}">
+                <img class="imagemQuizzesDoServidor"  src="${todosQuizzesDoServidor[i].image}" >
             </div>`
         }
     } )
 };
+
+function abreQuizz(id){
+    axios.get(`https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes/${id}`)
+    .then((qui)=> {
+        quizzClicado = qui.data;
+        console.log(quizzClicado);
+
+    }) 
+}
 
 renderizaQuizzesDoServidor();
 
